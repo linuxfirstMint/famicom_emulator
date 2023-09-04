c1cf11336e6e2520387d9ca28b63fa75becd589f
flm_10001 <firstlinuxmint@gmail.com>
2023-09-04 12:32:32 +0900

    [Refactor #32] Change mutable ref to immutable ref

--- src/cpu.rs
+++ src/cpu.rs
@@ -69,7 +69,7 @@ impl CPU {
         self.memory[addr as usize]
     }
 
-    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+    fn mem_read_u16(&self, pos: u16) -> u16 {
         let lo = self.mem_read(pos) as u16;
         let hi = self.mem_read(pos + 1) as u16;
         (hi << 8) | (lo as u16)
