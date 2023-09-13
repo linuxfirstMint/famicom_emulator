3154644accc23cbe5a9e71e22a8e265b58cade02
flm_10001 <firstlinuxmint@gmail.com>
2023-09-13 09:46:58 +0900

    [Impl #84] add rol

--- src/cpu.rs
+++ src/cpu.rs
@@ -281,6 +281,30 @@ impl CPU {
         }
     }
 
+    fn rol(&mut self, mode: &AddressingMode) {
+        let value = self.fetch_data(mode);
+
+        let carry_bit = (value >> 7) & 1;
+
+        let (mut rotated_value, _) = value.overflowing_shl(1);
+
+        rotated_value = rotated_value | carry_bit;
+
+        self.update_zero_and_negative_flags(value);
+
+        self.status
+            .set(ProcessorStatus::CARRY, (value >> 7) & 1 > 0);
+
+        match mode {
+            AddressingMode::Accumulator => self.accumulator = rotated_value,
+            _ => {
+                let addr = self.get_operand_address(mode);
+                self.mem_write(addr, rotated_value)
+            }
+        }
+    }
+
+
     fn status_bit(&self, reg: &ProcessorStatus) -> u8 {
         self.status.bits() & reg.bits()
     }
@@ -332,6 +356,7 @@ impl CPU {
                 ORA => self.ora(&opcode.mode),
                 ASL => self.asl(&opcode.mode),
                 LSR => self.lsr(&opcode.mode),
+                ROL => self.rol(&opcode.mode),
                 BRK => return,
                 _ => todo!(),
             }
@@ -941,6 +966,37 @@ mod tests {
                 cpu.run();
 
                 assert_eq!(cpu.mem_read(0x10), 0b00110101);
+                assert_eq!(cpu.status.contains(ProcessorStatus::CARRY), false);
+                assert_eq!(cpu.status.contains(ProcessorStatus::NEGATIVE), false);
+            }
+        }
+        mod rol {
+            use super::*;
+
+            #[test]
+            fn test_rol_load_acc() {
+                let mut cpu = CPU::new();
+
+                cpu.load(vec![0x2A, 0x00]);
+                cpu.reset();
+                cpu.accumulator = 0b10101011;
+                cpu.run();
+
+                assert_eq!(cpu.accumulator, 0b01010111);
+                assert_eq!(cpu.status.contains(ProcessorStatus::CARRY), true);
+                assert_eq!(cpu.status.contains(ProcessorStatus::NEGATIVE), true);
+            }
+
+            #[test]
+            fn test_rol_load_mem() {
+                let mut cpu = CPU::new();
+
+                cpu.mem_write(0x10, 0b01100101);
+                cpu.load(vec![0x26, 0x10, 0x00]);
+                cpu.reset();
+                cpu.run();
+
+                assert_eq!(cpu.mem_read(0x10), 0b11001010);
                 assert_eq!(cpu.status.contains(ProcessorStatus::CARRY), false);
                 assert_eq!(cpu.status.contains(ProcessorStatus::NEGATIVE), false);
             }
