18f611465119f63d4bfb6d0689da98abd3aeab06
flm_10001 <firstlinuxmint@gmail.com>
2023-10-18 03:19:59 +0000

    [feat #134] add Loading of rom files from main

--- src/bus.rs
+++ src/bus.rs
@@ -1,17 +1,32 @@
-use crate::cpu::{self, Mem};
+use core::panic;
+
+use crate::cpu::Mem;
+use crate::rom::Rom;
 
 pub struct Bus {
     pub cpu_vram: [u8; 2048],
+    pub rom: Rom,
 }
 
 impl Bus {
-    pub fn new() -> Self {
+    pub fn new(rom: Rom) -> Self {
         Self {
             cpu_vram: [0; 2048],
+            rom: rom,
         }
+    }
+
+    fn read_prg_rom(&self, mut addr: u16) -> u8 {
+        addr -= PRG;
+        if self.rom.prg_rom.len() == 0x4000 && addr >= 0x4000 {
+            addr -= 0x4000;
+        }
+        self.rom.prg_rom[addr as usize]
     }
 }
 
+const PRG: u16 = 0x8000;
+const PRG_ROM_END: u16 = 0xFFFF;
 const RAM: u16 = 0x0000;
 const RAM_MIRROR_END: u16 = 0x1FFF;
 const PPU_REGISTERS: u16 = 0x2000;
@@ -20,6 +35,7 @@ const PPU_REGISTERS_MIRROR_END: u16 = 0x3FFF;
 impl Mem for Bus {
     fn mem_read(&self, addr: u16) -> u8 {
         match addr {
+            PRG..=PRG_ROM_END => self.read_prg_rom(addr),
             RAM..=RAM_MIRROR_END => {
                 let mirror_down_addr = addr & 0b0000_0111_1111_1111;
                 self.cpu_vram[mirror_down_addr as usize]
@@ -36,6 +52,7 @@ impl Mem for Bus {
     }
     fn mem_write(&mut self, addr: u16, data: u8) {
         match addr {
+            PRG..=PRG_ROM_END => panic!("Attemt to write to Cartrige Rom Spase addr:{:X}", addr),
             RAM..=RAM_MIRROR_END => {
                 let mirror_down_addr = addr & 0b0000_0111_1111_1111;
                 self.cpu_vram[mirror_down_addr as usize] = data;
@@ -49,13 +66,23 @@ impl Mem for Bus {
         }
     }
 }
+
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
+
+    fn test_rom() -> Rom {
+        let game_dir = "roms";
+        let game_file = "snake.nes";
+        let game = fs::read(format!("{}/{}", game_dir, game_file)).unwrap();
+        Rom::new(&game).unwrap()
+    }
 
     #[test]
     fn test_mem_read_ram() {
-        let bus = Bus::new();
+        let bus = Bus::new(test_rom());
         let addr = 0x0000;
         let data = bus.mem_read(addr);
         assert_eq!(data, 0);
@@ -63,7 +90,7 @@ mod tests {
 
     #[test]
     fn test_mem_read_ram_mirror() {
-        let bus = Bus::new();
+        let bus = Bus::new(test_rom());
         let addr = 0x0800;
         let data = bus.mem_read(addr);
         assert_eq!(data, 0);
@@ -81,7 +108,7 @@ mod tests {
 
     #[test]
     fn test_mem_read_invalid_address() {
-        let bus = Bus::new();
+        let bus = Bus::new(test_rom());
         let addr = 0xFFFF;
         let data = bus.mem_read(addr);
         assert_eq!(data, 0);
@@ -89,7 +116,7 @@ mod tests {
 
     #[test]
     fn test_mem_write_ram() {
-        let mut bus = Bus::new();
+        let mut bus = Bus::new(test_rom());
         let addr = 0x0000;
         let data = 0x42;
         bus.mem_write(addr, data);
@@ -98,7 +125,7 @@ mod tests {
 
     #[test]
     fn test_mem_write_ram_mirror() {
-        let mut bus = Bus::new();
+        let mut bus = Bus::new(test_rom());
         let addr = 0x0800;
         let data = 0x42;
         bus.mem_write(addr, data);
@@ -106,12 +133,11 @@ mod tests {
     }
 
     #[test]
+    #[should_panic]
     fn test_mem_write_invalid_address() {
-        let mut bus = Bus::new();
+        let mut bus = Bus::new(test_rom());
         let addr = 0xFFFF;
         let data = 0x42;
-        let result = bus.mem_write(addr, data);
-        // shoud not panic
-        assert_eq!(result, ());
+        bus.mem_write(addr, data)
     }
 }
